5ac9cb2802559deab5211a9e8aa2934702775fea
Sameer Ali Butt <108467884+sameerbutt28@users.noreply.github.com>
2023-05-19 01:24:21 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+import NavBar from "./component/navbar";
+import Main from "./component/main";
+
+export default function App()
+{
+    return(
+        <div className="conatianer">
+            <NavBar />
+            <Main />
+        </div>
+    )
+}
