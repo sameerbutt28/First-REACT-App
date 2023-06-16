9b77b99ba5318c94f0cfea33763af796d13315d4
Sameer Ali Butt <108467884+sameerbutt28@users.noreply.github.com>
2023-06-17 00:23:42 +0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import NavBar from "./component/navbar";
+import Main from "./component/main";
+
+
+function App() {
+  return (
+    <div className="container">
+      <NavBar/>
+      <Main/>
+    </div>
+  );
+}
+
+export default App;
